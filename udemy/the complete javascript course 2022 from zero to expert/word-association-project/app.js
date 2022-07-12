1dad461945e920002c19758c03510da7826fab85
tafreha <tafreha.shaban@gmail.com>
2022-07-12 18:18:44 +0300

    save changes react

---
+++ udemy/the complete javascript course 2022 from zero to expert/word-association-project/app.js
@@ -0,0 +1,30 @@
+const scoreDisplay = document.getElementById('score-display')
+const questionDisplay = document.getElementById('question-display')
+
+const question = [{
+        quize: ['value', 'estimate', 'evaluate'],
+        option: ['jury', 'assess'],
+        correct: 2,
+    },
+    {
+        quize: ['close', 'near', 'next'],
+        option: ['trace', 'adjent'],
+        correct: 1,
+    },
+    {
+        quize: ['foreign', 'national', 'ethnic'],
+        option: ['mad', 'exotic'],
+        correct: 2,
+    },
+    {
+        quize: ['assume', 'insight', 'weather'],
+        option: ['forecast', 'sustationable'],
+        correct: 1,
+    },
+    {
+        quize: ['fast', 'quick', 'prompt'],
+        option: ['charity', 'rapid'],
+        correct: 2,
+    },
+
+]
